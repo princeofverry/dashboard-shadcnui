93086d6bad885f8f564e32dd963a11468f3e7bf7
princeofverry <verrykurniawan@students.undip.ac.id>
2024-07-02 19:38:04 +0700

    fix: side navbar

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -1,10 +1,7 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Nav } from "./ui/nav";
-
-type Props = {};
-
 import {
   ShoppingCart,
   LayoutDashboard,
@@ -13,9 +10,18 @@ import {
   ChevronRight,
 } from "lucide-react";
 import { Button } from "./ui/button";
+import { useWindowWidth } from "@react-hook/window-size";
+
+type Props = {};
 
 export default function SideNavbar({}: Props) {
   const [isCollapsed, setIsCollapsed] = useState(false);
+  const onlyWidth = useWindowWidth();
+  const [mobileWidth, setMobileWidth] = useState(false);
+
+  useEffect(() => {
+    setMobileWidth(onlyWidth < 768);
+  }, [onlyWidth]);
 
   function toggleSidebar() {
     setIsCollapsed(!isCollapsed);
@@ -23,17 +29,19 @@ export default function SideNavbar({}: Props) {
 
   return (
     <div className="relative min-w-[80px] border-r px-3 pb-10 pt-24">
-      <div className="absolute right-[-20px] top-7">
-        <Button
-          onClick={toggleSidebar}
-          variant="secondary"
-          className="rounded-full p-2"
-        >
-          <ChevronRight />
-        </Button>
-      </div>
+      {!mobileWidth && (
+        <div className="absolute right-[-20px] top-7">
+          <Button
+            onClick={toggleSidebar}
+            variant="secondary"
+            className="rounded-full p-2"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
       <Nav
-        isCollapsed={isCollapsed}
+        isCollapsed={mobileWidth ? true : isCollapsed}
         links={[
           {
             title: "Dashboard",
